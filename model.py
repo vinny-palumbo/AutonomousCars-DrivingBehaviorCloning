f1d762c5f45d1f5bd5ae21d6e019c9c381d5db5b
Vinny Palumbo <vinnypalumbo.com@gmail.com>
2017-04-11 03:59:12 +0000

    Implement LeNet Convolutional Neural Network architecture

--- model.py
+++ model.py
@@ -23,15 +23,25 @@ X_train = np.array(images)
 y_train = np.array(measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Flatten, Dense, Lambda, Activation
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
-model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160, 320, 3)))
+model.add(Convolution2D(6,5,5,activation="relu"))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Convolution2D(16,5,5,activation="relu"))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten())
+model.add(Dense(120))
+model.add(Activation('relu'))
+model.add(Dense(84))
+model.add(Activation('relu'))
 model.add(Dense(1))
-
+ 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=3)
+model.fit(X_train, y_train, batch_size=64, nb_epoch=5, validation_split=0.2, shuffle=True)
 
 model.save('model.h5')
 exit()
