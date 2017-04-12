92c2de444458fb64687f825e778280f21910544c
Vinny <vin_palumbo@hotmail.com>
2017-04-12 00:20:18 -0400

    Add Left and Right Cameras data and train the model to correct to the center when car is near the sides of the road

--- model.py
+++ model.py
@@ -11,13 +11,21 @@ with open('data/driving_log.csv') as csvfile:
 images = []  
 measurements = []
 for line in lines:
-    source_path = line[0]
-    filename = source_path.split('\\')[-1]
-    current_path = 'data/IMG/' + filename
-    image = cv2.imread(current_path)
-    images.append(image)
-    measurement = float(line[3])
-    measurements.append(measurement)
+    correction = 0.2 # this is a parameter to tune
+    steering_center = float(line[3])
+    for i in range(3):
+        source_path = line[i]
+        filename = source_path.split('\\')[-1]
+        current_path = 'data/IMG/' + filename
+        image = cv2.imread(current_path)
+        images.append(image)
+        if i == 0:
+            measurement = steering_center
+        elif i == 1:
+            measurement = steering_center + correction
+        else:
+            measurement = steering_center - correction
+        measurements.append(measurement)
         
 augmented_images, augmented_measurements = [], []
 for image, measurement in zip(images, measurements):
@@ -48,7 +56,7 @@ model.add(Activation('relu'))
 model.add(Dense(1))
  
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, batch_size=64, nb_epoch=5, validation_split=0.2, shuffle=True)
+model.fit(X_train, y_train, batch_size=64, nb_epoch=3, validation_split=0.2, shuffle=True)
 
 model.save('model.h5')
 exit()
