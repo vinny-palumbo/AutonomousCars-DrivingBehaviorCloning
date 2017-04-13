58e3891ade263ff7d49c4cc361b09520fd348007
Vinny <vin_palumbo@hotmail.com>
2017-04-12 20:25:32 -0400

    Cropped top and bottom pixels from images outputted by cameras, so that model doesn't focus on car hood, sky, trees, etc.

--- model.py
+++ model.py
@@ -38,12 +38,13 @@ X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Activation
+from keras.layers import Flatten, Dense, Lambda, Activation, Cropping2D
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
 model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((70,25),(0,0))))
 model.add(Convolution2D(6,5,5,activation="relu"))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Convolution2D(16,5,5,activation="relu"))
